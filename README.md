8dab5f6270f76f0749587141e4685b1daaf6b605
Aditya Pujari <apujari@hippo.com>
2022-02-22 16:02:27 -0600

    Setting up python package.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Salesforce Prometheus Exporter
+
+This repo fetches logs from Salesforce & creates a custom exporter, which can be scraped by prometheus.
+
+### Getting Started
+
+```shell
+pip install salesforce-prometheus-exporter
+```
+
+Set the following environment variables.
+```shell
+SF_URL=<salesforce url>
+SF_VERSION=<salesforce version>
+CONSUMER_ID=<salesforce consumer/client ID>
+CONSUMER_SECRET=<salesforce consumer/client secret>
+AUTH_USERNAME=<salesforce username>
+AUTH_PASSWORD=<salesforce password>
+```
+Then, start server command:
+
+```shell
+salesforce-exporter start-server
+```
+Go to http://localhost:3000/metrics to view the metrics.
+
+NOTE: The metrics will start with the prefix `sfdc`
---
+++ src/cli/main.py
@@ -0,0 +1,44 @@
+from click import group, pass_context, option, INT
+from pkg_resources import iter_entry_points
+from click_plugins import with_plugins
+
+from flask import Flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from wsgiref.simple_server import make_server
+
+
+app = Flask(__name__)
+
+
+@with_plugins(iter_entry_points("click_command_tree"))
+@group()
+@pass_context
+def main(context):
+    """
+    main entry point for salesforce exporter
+    """
+    pass
+
+
+def health(env, start_response):
+    start_response("200 OK", [("Content-Type", "text/html")])
+    return [b"OK"]
+
+def home(env, start_response):
+    start_response("200 OK", [("Content-Type", "text/html")])
+    return [b"<h1> Salesforce Exporter (SFDC) </h1>"]
+
+@main.command("start-server")
+@pass_context
+@option("--port", default=3000, type=INT)
+def server(context, port):
+    """
+    Starting wsgi server for prometheus exporter.
+    """
+
+    app.wsgi_app = DispatcherMiddleware(
+        app.wsgi_app, {"/": home, "/health": health }
+    )
+
+    httpd = make_server(host="", port=port, app=app)
+    httpd.serve_forever()
